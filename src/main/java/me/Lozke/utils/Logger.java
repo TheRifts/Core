6ba6cf37fd4de00a459b649831bbda22e89dae9f
Lozke <17802931+Lozke@users.noreply.github.com>
2020-08-14 14:39:55 -0500

    Upgrade Logger Util

--- src/main/java/me/Lozke/utils/Logger.java
+++ src/main/java/me/Lozke/utils/Logger.java
@@ -4,19 +4,52 @@
 package me.Lozke.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Level;
 
 public class Logger {
 
-    public static final String prefix = "[Agorian Rifts] ";
-    public static final java.util.logging.Logger logger = Bukkit.getServer().getLogger();
+    private static final String PREFIX_REGEX = "[%plugin_name%] ";
+    private static final String DEFAULT_PREFIX = "[Agorian Rifts] ";
+    private static final java.util.logging.Logger logger = Bukkit.getServer().getLogger();
 
+    private static String getPrefix(Plugin plugin) {
+        return PREFIX_REGEX.replace("%plugin_name%", plugin.getName());
+    }
+
+    public static void log(Level level, Plugin plugin, String message) {
+        logger.log(level, getPrefix(plugin) + message);
+    }
+    public static void log(Level level, String message) {
+        logger.log(level, DEFAULT_PREFIX + message);
+    }
+
+    public static void log(Plugin plugin, String message) {
+        logger.info(getPrefix(plugin) + message);
+    }
     public static void log(String message) {
-        logger.info(prefix + message);
+        logger.info(DEFAULT_PREFIX + message);
     }
 
+    public static void broadcast(Plugin plugin, String message) {
+        logger.info(getPrefix(plugin) + message);
+    }
     public static void broadcast(String message) {
-        Bukkit.broadcastMessage(prefix + message);
+        Bukkit.broadcastMessage(DEFAULT_PREFIX + message);
     }
 
-    //TODO add warn/error support for logger
+    public static void warn(String message) {
+        logger.log(Level.WARNING, DEFAULT_PREFIX + message);
+    }
+    public static void warn(Plugin plugin, String message) {
+        logger.log(Level.WARNING, getPrefix(plugin) + message);
+    }
+
+    public static void error(String message) {
+        logger.log(Level.SEVERE, DEFAULT_PREFIX + message);
+    }
+    public static void error(Plugin plugin, String message) {
+        logger.log(Level.SEVERE, getPrefix(plugin) + message);
+    }
 }
