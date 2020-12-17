6b6a97c60eb8612b7be3f71171882af8c45e2879
Lozke <17802931+Lozke@users.noreply.github.com>
2020-12-17 01:23:50 -0600

    Added attack cooldown on mobs to prevent them frame locking players

--- src/main/java/me/Lozke/listeners/DamagedEntityListener.java
+++ src/main/java/me/Lozke/listeners/DamagedEntityListener.java
@@ -3,14 +3,20 @@ package me.Lozke.listeners;
 import me.Lozke.AgorianRifts;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.Plugin;
+
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class DamagedEntityListener implements Listener {
 
     private Plugin plugin;
+    private final Map<LivingEntity, Long> MONSTER_HIT_COOLDOWN = new WeakHashMap<>();
 
     public DamagedEntityListener(AgorianRifts plugin) {
         this.plugin = plugin;
@@ -31,4 +37,23 @@ public class DamagedEntityListener implements Listener {
         }, 0L);
     }
 
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof LivingEntity)) return;
+        if (!(event.getDamager() instanceof Player) && !canMonsterHit((LivingEntity) event.getDamager())) {
+            event.setCancelled(true);
+            return;
+        }
+        putMonsterHit((LivingEntity) event.getDamager());
+    }
+
+    private boolean canMonsterHit(LivingEntity entity) {
+        return System.currentTimeMillis() > MONSTER_HIT_COOLDOWN.getOrDefault(entity, 0L);
+    }
+
+    public void putMonsterHit(LivingEntity livingEntity) {
+        if (!(livingEntity instanceof Player)) {
+            MONSTER_HIT_COOLDOWN.put(livingEntity, System.currentTimeMillis() + 400);
+        }
+    }
 }
