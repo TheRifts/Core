0638af60fd5647a66d80de2643923e9c15153297
Lozke <17802931+Lozke@users.noreply.github.com>
2020-12-17 01:29:24 -0600

    Removed extra white space

--- src/main/java/me/Lozke/data/Rarity.java
+++ src/main/java/me/Lozke/data/Rarity.java
@@ -8,22 +8,19 @@ public enum Rarity {
     UNCOMMON("II", new ItemStack(Material.LIME_CONCRETE), "&2"),
     RARE("III", new ItemStack(Material.LIGHT_BLUE_CONCRETE), "&b"),
     UNIQUE("IV", new ItemStack(Material.PURPLE_CONCRETE), "&5"),
-    ANCIENT("V", new ItemStack(Material.YELLOW_CONCRETE), "&e"); 
-
+    ANCIENT("V", new ItemStack(Material.YELLOW_CONCRETE), "&e");
 
     public static Rarity[] types = Rarity.values();
 
     private final String symbol;
     private final ItemStack icon;
     private final String colorCode;
-
-
+    
     Rarity(String symbol, ItemStack icon, String colorCode) {
         this.symbol = symbol;
         this.icon = icon;
         this.colorCode = colorCode;
     }
-
 
     public String getSymbol() {
         return symbol;
