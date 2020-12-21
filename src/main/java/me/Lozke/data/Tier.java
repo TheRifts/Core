d871857036fc12b597c7c1260abf282253ed7776
Lozke <17802931+Lozke@users.noreply.github.com>
2020-12-20 19:36:31 -0600

    2k durability

--- src/main/java/me/Lozke/data/Tier.java
+++ src/main/java/me/Lozke/data/Tier.java
@@ -3,12 +3,12 @@ package me.Lozke.data;
 import org.bukkit.Material;
 
 public enum Tier {
-    T1(1, "WHITE", Material.WHITE_CONCRETE,"LEATHER", "WOODEN", "&7", "Shoddy", 100), //Wood/Leather
-    T2(2, "LIME", Material.LIME_CONCRETE,"CHAINMAIL", "STONE", "&2", "Ordinary", 250), //Stone/Chain
-    T3(3, "LIGHT_BLUE", Material.LIGHT_BLUE_CONCRETE,"IRON", "IRON","&b", "Strong", 600), //Steel
-    T4(4, "PURPLE", Material.PURPLE_CONCRETE,"DIAMOND","DIAMOND","&5", "Fantastic", 850), //Ivory
-    T5(5, "YELLOW", Material.YELLOW_CONCRETE,"GOLDEN","GOLDEN", "&e", "Incredible", 1000), //Heavenly
-    T6(6, "BLACK", Material.BLACK_CONCRETE, "NETHERITE", "NETHERITE", "&c", "Super Stronk", 1250); //T6 Demonic
+    T1(1, "WHITE", Material.WHITE_CONCRETE,"LEATHER", "WOODEN", "&7", "Shoddy", 2000), //Wood/Leather
+    T2(2, "LIME", Material.LIME_CONCRETE,"CHAINMAIL", "STONE", "&2", "Ordinary", 2000), //Stone/Chain
+    T3(3, "LIGHT_BLUE", Material.LIGHT_BLUE_CONCRETE,"IRON", "IRON","&b", "Strong", 2000), //Steel
+    T4(4, "PURPLE", Material.PURPLE_CONCRETE,"DIAMOND","DIAMOND","&5", "Fantastic", 2000), //Ivory
+    T5(5, "YELLOW", Material.YELLOW_CONCRETE,"GOLDEN","GOLDEN", "&e", "Incredible", 2000), //Heavenly
+    T6(6, "BLACK", Material.BLACK_CONCRETE, "NETHERITE", "NETHERITE", "&c", "Super Stronk", 2000); //T6 Demonic
 
     public static Tier[] types = Tier.values();
 
